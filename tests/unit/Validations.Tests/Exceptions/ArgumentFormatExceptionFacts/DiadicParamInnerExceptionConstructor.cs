7ed9e290cedac72a35aeb43f827652652fcd94ab
Lorenzo Solano Martinez <lorenzo.sm@gmail.com>
2021-11-04 15:21:24 -0400

    #17 Adding unit tests.

---
+++ tests/unit/Validations.Tests/Exceptions/ArgumentFormatExceptionFacts/DiadicParamInnerExceptionConstructor.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+using Triplex.Validations.Exceptions;
+
+namespace Triplex.Validations.Tests.Exceptions.ArgumentFormatExceptionFacts
+{
+    [TestFixture]
+    internal sealed class DiadicParamInnerExceptionConstructor
+    {
+        [Test]
+        public void Throws_Nothing(
+            [ValueSource(typeof(TriadicConstructorFacts), nameof(TriadicConstructorFacts.ValidParameterNames))] string? paramName,
+            [ValueSource(typeof(TriadicConstructorFacts), nameof(TriadicConstructorFacts.ValidInnerExceptions))] Exception? innerException)
+            => Assert.That(() => new ArgumentFormatException(paramName!, innerException!), Throws.Nothing);
+    }
+}
---
+++ tests/unit/Validations.Tests/Exceptions/ArgumentFormatExceptionFacts/TriadicConstructorFacts.cs
@@ -0,0 +1,26 @@
+using System;
+using NUnit.Framework;
+using Triplex.Validations.Exceptions;
+
+namespace Triplex.Validations.Tests.Exceptions.ArgumentFormatExceptionFacts
+{
+    [TestFixture]
+    internal sealed class TriadicConstructorFacts
+    {
+        internal static readonly string?[] ValidParameterNames
+            = new string?[] { null, "", " ", "i'm a weird param name", "firstname" };
+
+        internal static readonly string?[] ValidMessages
+            = new string?[] { null, "", " ", "Some error message" };
+
+        internal static readonly Exception?[] ValidInnerExceptions
+            = new Exception?[] { null, new Exception() };
+
+        [Test]
+        public void Throws_Nothing(
+            [ValueSource(nameof(ValidParameterNames))] string? paramName,
+            [ValueSource(nameof(ValidMessages))] string? message,
+            [ValueSource(nameof(ValidInnerExceptions))] Exception? innerException)
+            => Assert.That(() => new ArgumentFormatException(paramName!, message!, innerException!), Throws.Nothing);
+    }
+}
